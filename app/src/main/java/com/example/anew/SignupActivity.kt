36d376d92cccc580453413d17b97938f39a3ed37
Markus <markuskristoffer@gmail.com>
2023-10-26 20:45:27 +0800

    New Project. Body & Layouts. (Updated | Full functioning nav | Images and labels)

--- app/src/main/java/com/example/anew/SignupActivity.kt
+++ app/src/main/java/com/example/anew/SignupActivity.kt
@@ -1,11 +1,47 @@
 package com.example.anew
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import com.example.anew.databinding.ActivitySignupBinding
 
-class SignupActivity : AppCompatActivity() {
+class   SignupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignupBinding
+    private lateinit var databaseHelper: DatabaseHelper
+    private lateinit var data: LogIn_SignUpDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        databaseHelper = DatabaseHelper(this)
+        binding.signupButton.setOnClickListener {
+
+            val signupUsername = binding.signupUsername.text.toString()
+            val signupPassword = binding.signupPassword.text.toString()
+            val savedata = data.insertdata(signupUsername, signupPassword)
+
+            if (TextUtils.isEmpty(signupPassword) || TextUtils.isEmpty(signupUsername)){
+                Toast.makeText(this, "Add Username or Password", Toast.LENGTH_SHORT).show()
+            }else{
+                if (savedata == true) {
+                    Toast.makeText(this, "Sign Up Successful", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+
+
+        }
+        binding.loginRedirect.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
+
 }
