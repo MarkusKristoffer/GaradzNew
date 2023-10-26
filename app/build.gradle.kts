3da296d887dfa9c19baaf852d886d3e324a65a92
Markus <markuskristoffer@gmail.com>
2023-10-26 11:43:14 +0800

    New Project. Body & Layouts. (Updated | Full functioning nav)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -46,4 +49,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("androidx.activity:activity-compose:1.3.0")
+    implementation ("androidx.compose.foundation:foundation:1.0.0")
+    implementation ("androidx.activity:activity:1.6.0-alpha04")
 }
--- app/src/main/java/com/example/anew/HomeFragment.kt
+++ app/src/main/java/com/example/anew/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.FragmentTransaction
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,8 +35,17 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        var view = inflater.inflate(R.layout.fragment_home, container, false)
+        view.findViewById<TextView>(R.id.tv_seeall).setOnClickListener{
+            openFragment(AllLocationsFragment())
+        }
+        return view
+    }
+
+    private fun openFragment(fragment: Fragment){
+        val fragmentTransaction: FragmentTransaction? = fragmentManager?.beginTransaction()
+        fragmentTransaction?.replace(R.id.fragment_container, fragment)
+        fragmentTransaction?.commit()
     }
 
     companion object {
